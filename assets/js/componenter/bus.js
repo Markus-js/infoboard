8f1a15f5c69f83aa659079cb836dc307b122dce8
Jannick Nielsen <jannicktrollenielsen@hotmail.com>
2022-03-16 14:29:08 +0100

    broken

--- assets/js/componenter/bus.js
+++ assets/js/componenter/bus.js
@@ -1,3 +1,49 @@
-export const bus = () => {console.log("bus.js is active")};
+export const bus = () => {};
 
-document.getElementById("bus").innerHTML = "Hello world! :D";
+doRequest();
+
+async function doRequest(){
+    const url = "https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=8519734&format=json";
+    let res = await fetch(url);
+
+    if (res.ok) {
+        let json = await res.json();
+        
+         build(json); 
+    } else {
+        return `HTTP error: ${res.status}`;
+    }
+}
+
+const busWrapper = document.createElement('div');
+
+    const busHeader = document.createElement('h2');
+    busHeader.innerText="BUSTIDER";
+    busWrapper.appendChild(busHeader);
+
+    const busList = document.createElement('section');
+    busWrapper.appendChild(busList);
+
+    //poster det til index.html
+    const post = document.getElementById('bus');
+    post.appendChild(busWrapper);
+
+function build(data) {
+    
+
+    for(let item of data.MultiDepartureBoard.Departure){
+        console.log(item.direction );
+
+        const busTap = document.createElement('artical');
+        busTap.innerHTML += `
+            <p>${item.line}</p>
+            <p>${item.direction}</p>
+            <p>${item.time}</p>
+        `;
+        busList.appendChild(busTap);
+    }    
+}
+
+setInterval(() => {
+    doRequest()
+}, 10000)
