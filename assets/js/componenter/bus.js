a21cda723667b3eef9a09b43d2977ea02287f5a5
Jannick Nielsen <jannicktrollenielsen@hotmail.com>
2022-03-17 11:46:03 +0100

    time is now in min

--- assets/js/componenter/bus.js
+++ assets/js/componenter/bus.js
@@ -1,20 +1,5 @@
 export const bus = () => {};
 
-doRequest();
-
-async function doRequest(){
-    const url = "https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=8519734&format=json";
-    let res = await fetch(url);
-
-    if (res.ok) {
-        let json = await res.json();
-        
-         build(json); 
-    } else {
-        return `HTTP error: ${res.status}`;
-    }
-}
-
 const busWrapper = document.createElement('div');
 
     const busHeader = document.createElement('h2');
@@ -28,17 +13,55 @@ const busWrapper = document.createElement('div');
     const post = document.getElementById('bus');
     post.appendChild(busWrapper);
 
+    
+
+
+    
+doRequest();
+
+async function doRequest(){
+    const url = "https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=8519734&format=json";
+    let res = await fetch(url);
+
+    if (res.ok) {
+        let json = await res.json();
+        
+         build(json);
+    } else {
+        return `HTTP error: ${res.status}`;
+    }
+}
+
 function build(data) {
     
 
     for(let item of data.MultiDepartureBoard.Departure){
-        console.log(item.direction );
+        const dateSplit = item.date.split(".");
+        const dateRestructure = dateSplit[1] + "-" + dateSplit[0] + "-" + dateSplit[2];
+
+        const busArriving  = Date.parse(`${dateRestructure} ${item.time}:00`);
+
+        const difference = busArriving - Date.now();
+    
+        const daysDifference = Math.ceil(difference/1000/60);
+
+        console.log(daysDifference);
+
+        let timeToBus = "";
+        
+        if(daysDifference < 60){
+            timeToBus = `${daysDifference} Min`;
+        }else{
+            let timeToBusHours = Math.trunc(daysDifference / 60);
+            let timeToBusMin = daysDifference % 60;
+            timeToBus = `${timeToBusHours}:${timeToBusMin} Timer`;
+        }
 
         const busTap = document.createElement('artical');
         busTap.innerHTML += `
             <p>${item.line}</p>
             <p>${item.direction}</p>
-            <p>${item.time}</p>
+            <p>${timeToBus}</p>
         `;
         busList.appendChild(busTap);
     }    
