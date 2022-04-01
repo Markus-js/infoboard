548b87fd64f9a0a6345412a7abfe4cb9a36571ff
Optic-Dex <danielthomsen238@gmail.com>
2022-04-01 10:57:30 +0200

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,8 @@ import { dateNTime } from "./componenter/dateNTime.js";
 dateNTime();
 
 import { weekly } from "./componenter/weekly.js";
-setInterval(weekly, 1800000)
-
 import { showdata, getNews } from "./componenter/Fetch.js";
-setInterval(showdata, 1800000)
-setInterval(getNews, 1800000)
+
 
 //bus
 import { bus } from "./componenter/bus.js";
@@ -16,6 +13,9 @@ bus()
 
 function refreshPage(){
     window.location.reload()
-}
 
+}
+getNews()
+showdata()
+weekly()
 setInterval(refreshPage, 3600000)
